a57787c3cdca0d14c3147eb905d5083a8ec28702
Matheus Dias <dias.math0@gmail.com>
2022-05-26 21:02:51 -0300

    Implementação das rotas

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Menu from "./components/Menu/Menu";
 
+//Routes
+import Router from "./router";
+
 function App() {
-  return <div className="App">
-    <Menu />
-    <Header />
-    <Footer />
-  </div>;
+  return (
+    <div className="App">
+      <Menu />
+      <Header />
+      <main>
+        <Router />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
