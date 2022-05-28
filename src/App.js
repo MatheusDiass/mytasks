ff5290a3970757d680324057304b116ee545c82a
Matheus Dias <dias.math0@gmail.com>
2022-05-28 14:22:16 -0300

    Login - Inicio da integração com o backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import "./assets/sass/app.sass";
 
+//Hooks
+import { useEffect } from "react";
+
 //Components
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Menu from "./components/Menu/Menu";
 
+//Helpers
+import { getCookie } from "./helpers/cookieManager";
+
 //Routes
 import Router from "./router";
 
 function App() {
+  let isLoggedIn = false;
+
+  function checkLogin() {
+    const user = getCookie("user");
+
+    if(user) {
+      isLoggedIn = true;
+    }
+  }
+
+  useEffect(() => {
+    checkLogin();
+    console.log(isLoggedIn);
+  });
+
   return (
     <div className="App">
       <Menu />
